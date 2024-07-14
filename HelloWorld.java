7b9df22ba63814247eccd408502789c09240bfae
alexx-pereira <jasp1@aluno.ifal.edu.br>
2024-07-14 14:50:54 -0300

    Exercicio_Hello-World

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+/* José Alex da Silva Pereira */
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.err.println("Hello, World!"); /* 14.07.2024 */
+    }
+    }
